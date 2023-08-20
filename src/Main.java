67851b65a1e44211548495d6aab66f48e6d79fbb
Ruslan <rif9rif@mail.ru>
2023-08-20 11:57:08 +0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+package com.itproger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Переменные и типы данных");
+
+        int age;
+        age = 56;
+        System.out.println("Возраст: " + age + ".");
+
+        age =5;
+        System.out.println(age);
+
+    }
+
+}
